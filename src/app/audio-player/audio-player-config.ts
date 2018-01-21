55df24f8b993b3c88e34ebb61380daba776219a1
Preston Lamb <preston.j.lamb@gmail.com>
2018-01-21 14:46:11 -0700

    remove invalid character

--- src/app/audio-player/audio-player-config.ts
+++ src/app/audio-player/audio-player-config.ts
@@ -1,4 +1,11 @@
 export interface AudioPlayerConfig {
-	timelineConfig?: { timelineColor: string; timelineHeight: number; playheadHeight: number; playheadColor: string };\
-	playPauseConfig?: { iconClassPrefix: string; playIconClass: string; pauseIconClass: string; iconPixelSize: number; playIconColor: string; pauseIconColor: string }
+	timelineConfig?: { timelineColor: string; timelineHeight: number; playheadHeight: number; playheadColor: string };
+	playPauseConfig?: {
+		iconClassPrefix: string;
+		playIconClass: string;
+		pauseIconClass: string;
+		iconPixelSize: number;
+		playIconColor: string;
+		pauseIconColor: string;
+	};
 }
